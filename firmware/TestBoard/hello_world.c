321eff3502c8ce6850f334284640e42dddc97110
Alexandre Tuleu <alexandre.tuleu.2005@gmail.com>
2021-03-28 19:25:12 +0200

    Displays clock

--- firmware/TestBoard/hello_world.c
+++ firmware/TestBoard/hello_world.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+static int i;
+
+void clock_init() {
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN,GPIO_OUT);
+
 	stdio_init_all();
+	i = 0;
+}
+
+void clock_out() {
+	gpio_put(PICO_DEFAULT_LED_PIN,i%2);
+	i++;
+	if ( (i % 2) == 0 ) {
+		printf("It is %d seconds\n",i/2);
+	}
+}
+
+int main() {
+	clock_init();
+	uint64_t period = 500e3;
+	uint64_t last = time_us_64();
+
 	while(true) {
-		printf("Hello, world!\n");
-		sleep_ms(1000);
+		uint64_t now = time_us_64();
+		if ( (now - last) >= period ) {
+			last += period;
+			clock_out();
+		}
 	}
 	return 0;
 }
