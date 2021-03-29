9b2a7672fd71cd4a1d9b6dcfa98ade883f0e6d76
Alexandre Tuleu <alexandre.tuleu.2005@gmail.com>
2021-03-29 16:26:24 +0200

    Makes LED shine !
    Let the LED shine !
    Let the LED shine In !

--- firmware/TestBoard/hello_world.c
+++ firmware/TestBoard/hello_world.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "tlc5973.pio.h"
+
 static int i;
+
+static inline void put_pixel(uint8_t r,uint8_t g, uint8_t b) {
+	pio_sm_put_blocking(pio0,
+						0,
+						(47 << 16) | (0x3aa << 4 ) | r >> 4);
+	pio_sm_put_blocking(pio0,
+						0,
+						(r << 28) | (g << 16) |(b << 4));
+}
 
 void clock_init() {
 	gpio_init(PICO_DEFAULT_LED_PIN);
@@ -15,7 +26,17 @@ void clock_out() {
 	gpio_put(PICO_DEFAULT_LED_PIN,i%2);
 	i++;
 	if ( (i % 2) == 0 ) {
+		put_pixel(0xff,0x00,0x00);
+		put_pixel(0x00,0xff,0x00);
+		put_pixel(0x00,0x00,0xff);
+		put_pixel(0xff,0xff,0x00);
+
 		printf("It is %d seconds\n",i/2);
+	} else {
+		put_pixel(0x00,0x00,0x00);
+		put_pixel(0x00,0x00,0x00);
+		put_pixel(0x00,0x00,0x00);
+		put_pixel(0x00,0x00,0x00);
 	}
 }
 
@@ -23,6 +44,12 @@ int main() {
 	clock_init();
 	uint64_t period = 500e3;
 	uint64_t last = time_us_64();
+
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &tlc5973_program);
+
+    tlc5973_program_init(pio0, 0, offset, 10, 2e6);
 
 	while(true) {
 		uint64_t now = time_us_64();
