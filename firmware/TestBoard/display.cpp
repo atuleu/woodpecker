66729f640d49a25799b0bf7ecee109796ef951eb
Alexandre Tuleu <alexandre.tuleu.2005@gmail.com>
2021-04-17 20:00:17 +0200

    Adds 8/12 bit color converter

--- firmware/TestBoard/display.cpp
+++ firmware/TestBoard/display.cpp
@@ -5,19 +5,20 @@
 #include "tlc5973.pio.h"
 
 #include <tuple>
+#include <functional>
+
+typedef std::tuple<uint8_t,uint8_t,uint8_t>    RGB8;
+typedef std::tuple<uint16_t,uint16_t,uint16_t> RGB16;
+typedef std::tuple<uint8_t,uint8_t>            Position;
 
 
-typedef std::tuple<uint8_t,uint8_t,uint8_t> RGBColor;
-typedef std::tuple<uint8_t,uint8_t>         Position;
-
-
-class LEDAdressable {
+class LEDAddressable {
 public:
-	virtual void SetPixel(size_t index, const RGBColor & color) = 0;
+	virtual void SetPixel(size_t index, const RGB16 & color) = 0;
 };
 
 template <size_t Size>
-class LEDStripQueue : public LEDAdressable {
+class LEDStripQueue : public LEDAddressable {
 public:
 	LEDStripQueue(PIO pio, uint sm, uint pin)
 		: d_index(Size*2)
@@ -32,13 +33,10 @@ public:
 	}
 
 	size_t SendAvailable() {
-		//		printf("Sending...");
 		while( d_index < 2*Size  ) {
-			//	printf(" %d",d_index);
 			pio_sm_put_blocking(d_pio,d_sm,d_data[d_index]);
 			++d_index;
 		}
-		//		printf("DONE\n");
 		return 2*Size - d_index;
 	}
 
@@ -46,11 +44,11 @@ public:
 		d_index = 0;
 	}
 
-	void SetPixel(size_t index, const RGBColor & color) override {
-		d_data[2*index + 0] = (47 << 16) | ( 0x3aa << 4 ) | (std::get<0>(color) >> 4);
-		d_data[2*index + 1] = ( std::get<0>(color) << 28 ) | ( 0xf << 24 )
-			| ( std::get<1>(color) << 16 ) | ( 0xf << 12 )
-			| ( std::get<2>(color) << 4 ) | 0xf;
+	void SetPixel(size_t index, const RGB16 & color) override {
+		d_data[2*index + 0] = (47 << 16) | ( 0x3aa << 4 ) | ( (std::get<0>(color) & 0xfff) >> 8);
+		d_data[2*index + 1] = ( std::get<0>(color) << 24 )
+			| ( ( std::get<1>(color) & 0xfff ) << 12 )
+			| ( std::get<2>(color) & 0xfff );
 	}
 
 private:
@@ -61,6 +59,25 @@ private:
 	uint32_t   d_data[Size*2];
 };
 
+typedef std::function<RGB16 (const RGB8 & )> GammaConverter;
+
+struct PixelData {
+	LEDAddressable * Queue;
+	size_t           Index;
+	GammaConverter   Gamma;
+};
+
+RGB16 convertKey(const RGB8 & c) {
+	return { uint16_t(std::get<0>(c)) << 2,
+			 uint16_t(std::get<1>(c)) << 1,
+			 uint16_t(std::get<2>(c)) << 2 };
+}
+
+RGB16 convertEncoder(const RGB8 & c ) {
+	return { uint16_t(std::get<0>(c)),
+			 uint16_t(std::get<1>(c))>> 1,
+			 uint16_t(std::get<2>(c))};
+}
 
 class DisplayImpl {
 public:
@@ -71,14 +88,19 @@ public:
 		for ( size_t y = 0; y < 4; ++y ) {
 			for (size_t x = 0; x < 5; ++x) {
 				size_t index = y * 5 + x;
-				d_queueMap[index] = &d_topQueue;
-				d_indexMap[index] = x * 4;
+				d_pixelData[index].Queue = &d_topQueue;
+				d_pixelData[index].Index = x * 4;
 				if ( (x % 2) == 0 ) {
-					d_indexMap[index] += 3 - y;
+					d_pixelData[index].Index += 3 - y;
 				} else {
-					d_indexMap[index] += y;
+					d_pixelData[index].Index += y;
 				}
-				d_indexMap[index] = 19 - d_indexMap[index];
+				if ( (y % 2) == 1 ) {
+					d_pixelData[index].Gamma = &convertKey;
+				} else {
+					d_pixelData[index].Gamma = &convertEncoder;
+				}
+				//				d_pixelData[index].Index = 19 - d_pixelData[index].Index;
 			}
 		}
 
@@ -86,13 +108,15 @@ public:
 		for ( size_t y = 4; y < 6; ++y ) {
 			for (size_t x = 0; x < 5; ++x) {
 				size_t index = y * 5 + x;
-				d_queueMap[index] = &d_bottomQueue;
-				d_indexMap[index] = x * 2;
+				d_pixelData[index].Queue = &d_bottomQueue;
+				d_pixelData[index].Index = x * 2;
 				if ( x % 2 == 1 ) {
-					d_indexMap[index] += 5 - y;
+					d_pixelData[index].Index += 5 - y;
 				} else {
-					d_indexMap[index] += y - 4;
+					d_pixelData[index].Index += y - 4;
 				}
+				//				d_pixelData[index].Index = 9 - d_pixelData[index].Index;
+				d_pixelData[index].Gamma = &convertKey;
 			}
 		}
 
@@ -102,7 +126,7 @@ public:
 		printf("-------------\n");
 		for ( size_t y = 0 ; y < 6; ++y) {
 			for ( size_t x = 0; x < 5; ++ x) {
-				printf("%d ",d_indexMap[y*5+x]);
+				printf("%d ",d_pixelData[y*5+x].Index);
 			}
 			printf("\n");
 		}
@@ -115,18 +139,21 @@ public:
 			d_last += PERIOD_US;
 			d_topQueue.ReArm();
 			d_bottomQueue.ReArm();
+			PrintIndex();
 		}
 		d_topQueue.SendAvailable();
 		d_bottomQueue.SendAvailable();
 	}
 
+
+
 	void SetPixel(const Position & position,
-				  const RGBColor & color) {
+				  const RGB8 & color) {
 		if ( std::get<0>(position) >= 5 || std::get<1>(position) >=6 ) {
 			return;
 		}
-		size_t pixelIndex = std::get<1>(position) * 5 + std::get<0>(position);
-		d_queueMap[pixelIndex]->SetPixel(d_indexMap[pixelIndex],color);
+		const auto & p = d_pixelData[std::get<1>(position) * 5 + std::get<0>(position)];
+		p.Queue->SetPixel(p.Index,p.Gamma(color));
 	}
 
 private:
@@ -136,8 +163,7 @@ private:
 	LEDStripQueue<20> d_topQueue;
 	LEDStripQueue<10> d_bottomQueue;
 
-	LEDAdressable * d_queueMap[30];
-	size_t          d_indexMap[30];
+	PixelData d_pixelData[30];
 };
 
 static DisplayImpl * s_impl = nullptr;
