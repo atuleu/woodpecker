8ecf3c65b85b89d4a06232c4dc8357597bcb6f53
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-09-23 10:00:42 +0200

    Update frame setup.

--- firmware/daughter/src/frame.h
+++ firmware/daughter/src/frame.h
@@ -4,74 +4,54 @@
 #include <stdint.h>
 
 struct __attribute__((packed)) Frame {
-	uint8_t ID : 6;
-
+	uint8_t  Type : 1;
+	uint8_t  ID : 2;
+	uint8_t  SequenceID : 3;
 	uint16_t Buttons : 10;
 
 	union {
 		uint8_t Fader[5];
 
 		struct __attribute__((packed)) {
-			uint8_t A : 4;
-			uint8_t B : 4;
+			// Absolute encoder state is keeped. We do not expect to see more
+			// than 16increment in a single ms(it is not event possible). Even
+			// with frame loss, getting 1 increment so keeping a 16bit index of
+			// the internal position is robust and fine.
+			uint8_t A : 4; // state should be absolute.
+			uint8_t B : 4; // state should be absolute.
 		} Encoder[5];
 	};
 };
 
-_Static_assert(sizeof(struct Frame) == 7, "Frame should be of size 7");
+_Static_assert(sizeof(struct Frame) == 7, "Frame should be 8 bytes long");
 
 enum FrameType {
 	ENCODER = 0,
 	FADER   = 1,
 };
 
-inline void Frame_set_ID(struct Frame *f, uint8_t ID, enum FrameType type) {
-	f->ID = ((uint8_t)type << 2) | (ID & 0x03);
-}
-
-inline void Frame_set_fader(struct Frame *f, uint8_t idx, uint8_t value) {
-	f->Fader[idx] = value;
-}
-
 inline void Frame_set_button(struct Frame *f, uint8_t idx, bool value) {
 	if (value == true) {
-		f->Buttons |= (1 << idx);
+		f->Buttons |= ((uint16_t)1 << idx);
 	} else {
-		f->Buttons &= ~(1 << idx);
+		f->Buttons &= ~((uint16_t)1 << idx);
 	}
 }
 
-inline uint8_t to_int4_t(int8_t v) {
-	if ((v & 0x80) != 0) {
-		return 0x08 | (v & 0x07);
-	} else {
-		return v & 0x07;
-	}
-}
-
-inline int8_t from_int4_t(uint8_t v) {
-	if ((v & 0x8) != 0) {
-		return 0x80 | (v & 0x07);
-	} else {
-		return v & 0x07;
-	}
-}
-
-inline void Frame_set_encoder(struct Frame *f, uint8_t idx, int8_t value) {
-
+inline void Frame_set_encoder(struct Frame *f, uint8_t idx, uint8_t value) {
 	// get the right index.
 	if ((idx & 0x01) == 0x00) {
-		f->Encoder[idx >> 1].A = to_int4_t(value);
+		f->Encoder[idx >> 1].A = value & 0x0f;
 	} else {
-		f->Encoder[idx >> 1].B = to_int4_t(value);
+		f->Encoder[idx >> 1].B = value & 0x0f;
 	}
 }
 
 int8_t Frame_get_encoder(const struct Frame *f, uint8_t idx) {
 	// get value from the right index.
 	if ((idx & 0x01) == 0x00) {
-		return from_int4_t(f->Encoder[idx >> 1].A);
+		return f->Encoder[idx >> 1].A;
 	} else {
-		return from_int4_t(f->Encoder[idx >> 1].B);
+		return f->Encoder[idx >> 1].B;
 	}
 }
